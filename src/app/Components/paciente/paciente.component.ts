9795f21b92bf2f90705b06c8b6e40ad74ccffcf0
Castorena43 <hector.castorena.castillo@gmail.com>
2020-03-12 09:33:40 -0600

    Add, update, delete paciente

--- src/app/Components/paciente/paciente.component.ts
+++ src/app/Components/paciente/paciente.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PacienteService } from 'src/app/services/paciente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-paciente',
@@ -9,33 +10,97 @@ import { PacienteService } from 'src/app/services/paciente.service';
 })
 export class PacienteComponent implements OnInit {
 
-  especialidades: any[] = [];
+  pacientes: Paciente[] = [];
   forma: FormGroup;
+  editar = false;
+  idx: number;
 
   constructor(private service: PacienteService) {
     this.forma = new FormGroup({
       'nombre': new FormControl('', [Validators.required,Validators.minLength(3)]),
-      'apellido_Paterno': new FormControl('', Validators.required),
-      'apellido_Materno': new FormControl('', Validators.required),
+      'apellido_paterno': new FormControl('', Validators.required),
+      'apellido_materno': new FormControl('', Validators.required),
       'nss': new FormControl('', Validators.required),
       'direccion': new FormControl('', Validators.required),
       'telefono': new FormControl('', Validators.required),
     });
 
-
+    this.service.getEspecialidades('api/paciente/all').subscribe( (data: any) => {
+      this.pacientes = data;
+      console.log(data);
+    });
 
    }
 
-  ngOnInit(): void {
-    this.service.getEspecialidades('api/paciente/all').subscribe( (data: any) => {
-      this.especialidades = data;
-      console.log(data);
+  ngOnInit(): void {}
+
+  create(){
+    if( this.forma.valid ){
+      //console.log(this.forma.value);
+      this.service.create('api/paciente/create',this.forma.value).subscribe( (data: any) => {
+        console.log(data);
+        this.alert('success','Agregado','Paciente agregado correctamente');
+      },
+      error => this.alert('error', 'Something went wrong!', 'Oops...')
+      );
+    }
+    console.log('create');
+  }
+
+  guardarCambios(idx: number) {
+    console.log('guardarCambios');
+    if( this.forma.valid ) {
+      // console.log(this.forma.value);
+      this.service.update('api/paciente/update/' + idx, this.forma.value).subscribe( (data: any) => {
+        console.log(data);
+        this.alert('success', 'Actualizado', 'Paciente actualizado correctamente');
+      },
+      error => {
+        this.alert('error', 'Something went wrong!', 'Oops...');
+        console.log(error);
+      }
+      );
+    }
+  }
+
+  Editar( paciente : Paciente ) {
+    this.editar = true;
+    this.forma.controls['nombre'].setValue(paciente.nombre);
+    this.forma.controls['apellido_paterno'].setValue(paciente.apellido_paterno);
+    this.forma.controls['apellido_materno'].setValue(paciente.apellido_materno);
+    this.forma.controls['nss'].setValue(paciente.nss);
+    this.forma.controls['direccion'].setValue(paciente.direccion);
+    this.forma.controls['telefono'].setValue(paciente.telefono);
+    //this.forma.controls['nombre'].setValue(paciente.nombre);
+    this.idx = paciente.id
+  }
+
+  Eliminar( id: number ) {
+    console.log(id);
+    this.service.delete('api/paciente/delete/' + id).subscribe( (data: any) => {
+      //console.log(data);
+      this.alert('success','Eliminado','Paciente eliminado correctamente');
+    },
+    error => this.alert('error', 'Something went wrong!', 'Oops...')
+    );
+  }
+
+  alert(icon: any, title: any, msg: any): void {
+    Swal.fire({
+      icon: icon,
+      title: title,
+      text: msg
     });
   }
 
-  guardarCambios(){
-    if( this.forma.valid ){
-      console.log(this.forma.value);
-    }
-  }
+}
+
+export interface Paciente {
+  id: number;
+  nombre: string;
+  apellido_paterno: string;
+  apellido_materno: string;
+  nss: number;
+  telefono: number;
+  direccion: string;
 }
--- src/app/services/paciente.service.ts
+++ src/app/services/paciente.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Paciente } from '../Components/paciente/paciente.component';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +12,19 @@ export class PacienteService {
 
   constructor( private http: HttpClient ) { }
 
-  getEspecialidades(path: string) {
-    return this.http.get(environment.apiBaseURL + path);
+  getEspecialidades(path: string) : Observable<Paciente> {
+    return this.http.get<Paciente>(environment.apiBaseURL + path);
+  }
+
+  create(path: string, data: Paciente) {
+    return this.http.post(environment.apiBaseURL + path, data);
+  }
+
+  delete(path: string) {
+    return this.http.delete(environment.apiBaseURL + path);
+  }
+
+  update(path: string, data: Paciente) {
+    return this.http.post(environment.apiBaseURL + path, data);
   }
 }
